ebd9dfc143a4addf8e8295047e16507a5f9d192f
Andreas <angohz@ymail.com>
2015-09-01 21:54:28 +0200

    Update Instructions_VM

--- include/Instructions_VM.hpp
+++ include/Instructions_VM.hpp
@@ -5,29 +5,46 @@
 
 //							num of params:
 /* SUPER-RISC INSTRUCTION SET */
+// stop program:
 const uint8_t STP = 0x00;	// 0
 
+// unconditional jump:
 const uint8_t JMP = 0x01;	// 1
+// jump if register C > 0:
 const uint8_t JGZ = 0x02;	// 1
+// jump if last operation caused overflow:
 const uint8_t JOF = 0x03;	// 1
-	 
+	
+// C = A + B:
 const uint8_t ADD = 0x04;	// 0
+// C = A - B:
 const uint8_t SUB = 0x05;	// 0
+// C = A & B:
 const uint8_t AND = 0x06;	// 0
+// C = A | B:
 const uint8_t BOR = 0x07;	// 0
+// C = A*2:
 const uint8_t SHL = 0x08;	// 0
+// C = A div 2:
 const uint8_t SHR = 0x09;	// 0
-	 
+
+// load from ram into A:
 const uint8_t LDA = 0x0A;	// 1
+// load from ram into B:
 const uint8_t LDB = 0x0B;	// 1
+// load constant value into A:
 const uint8_t LDC = 0x0C;	// 1
+// load 0 into B:
 const uint8_t LD0 = 0x0D;	// 0
  
+// store from C into ram:
 const uint8_t STR = 0x0E;	// 1
 
+// copy a value within ram:
 const uint8_t MOV = 0x0F;	// 2
 
-/* RISC ADDITIONAL INSTRUCTIONS */ // not yet implemented
+/* RISC ADDITIONAL INSTRUCTIONS */
+// no operation:
 const uint8_t NOP = 0x10;	// 0
 
 // jump if equal to zero:
@@ -35,9 +52,9 @@ const uint8_t JEZ = 0x12;	// 1
 // jump of no overflow:
 const uint8_t JNO = 0x13;	// 1
 
-// multiply
+// C = A * B
 const uint8_t MUL = 0x14;	// 0
-// integer-divide
+// C = A div B
 const uint8_t DIV = 0x15;	// 0
 
 // move C -> A
