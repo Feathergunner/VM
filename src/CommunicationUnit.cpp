9ab42274cd4cada8b6f2ccf93af30c2028add177
Andreas Goral <angohz@ymail.com>
2016-08-23 19:27:46 +0200

    Add implementation of Constructor for CommunicationUnit

--- src/CommunicationUnit.cpp
+++ src/CommunicationUnit.cpp
@@ -1,5 +1,10 @@
 #include "CommunicationUnit.hpp"
 
+CommunicationUnit::CommunicationUnit(int num_of_dev)
+{
+	number_of_extern_devices = num_of_dev;
+	channels.reserve(num_of_dev);
+}
 uint8_t CommunicationUnit::get_extern_value(int devicenumber, bool* succ)
 {
 	byte res = channels[devicenumber].read_byte(succ);
